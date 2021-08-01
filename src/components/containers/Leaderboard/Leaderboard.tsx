8339b01cdfb2f43567c34681081b1995dacdfb2f
Firas Jaber <firasjaberc@gmail.com>
2021-08-01 12:32:35 +0100

    leaderboard files setup

---
+++ src/components/containers/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayerTab from './PlayerTab';
+
+const Leaderboard = () => {
+	return (
+		<div>
+			<PlayerTab />
+			<PlayerTab />
+			<PlayerTab />
+		</div>
+	);
+};
+
+export default Leaderboard;
---
+++ src/components/containers/Leaderboard/PlayerTab.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayerTab = () => {
+	return <div>firrj - challengers</div>;
+};
+
+export default PlayerTab;
