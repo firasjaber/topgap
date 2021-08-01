f80a087e2f3137b68c945e0528f158e0491587ec
Firas Jaber <firasjaberc@gmail.com>
2021-08-01 13:26:49 +0100

    leaderboard container

--- src/components/containers/Leaderboard/Leaderboard.tsx
+++ src/components/containers/Leaderboard/Leaderboard.tsx
@@ -4,9 +4,18 @@ import PlayerTab from './PlayerTab';
 const Leaderboard = () => {
 	return (
 		<div>
-			<PlayerTab />
-			<PlayerTab />
-			<PlayerTab />
+			<div className="text-2xl font-semibold mb-3">
+				Players Leaderboard <span className="text-xl opacity-40 ml-2">EUW</span>
+			</div>
+			<div className="flex flex-col space-y-2">
+				<PlayerTab rank={1} name="FNC STAN" lp={1500} />
+				<PlayerTab rank={2} name="firrj" lp={1305} />
+				<PlayerTab rank={3} name="firrlXLZlmaoKurwa" lp={700} />
+				<PlayerTab rank={4} name="firrlXLZlmaoKurwa" lp={700} />
+				<PlayerTab rank={5} name="firrlXLZlmaoKurwa" lp={700} />
+				<PlayerTab rank={6} name="firrlXLZlmaoKurwa" lp={700} />
+				<PlayerTab rank={7} name="firrlXLZlmaoKurwa" lp={700} />
+			</div>
 		</div>
 	);
 };
--- src/components/containers/Leaderboard/PlayerTab.tsx
+++ src/components/containers/Leaderboard/PlayerTab.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import logo from './../../../utils/riot/emblems/Emblem_Challenger.png';
 
-const PlayerTab = () => {
-	return <div>firrj - challengers</div>;
+interface IPlayerTab {
+	rank: number;
+	name: string;
+	lp: number;
+}
+
+const PlayerTab: React.FC<IPlayerTab> = ({ rank, name, lp }) => {
+	const summonerIcon = 'https://i.pinimg.com/236x/40/a1/ac/40a1ac9579adba4b592a15bcea8f0e18.jpg';
+	return (
+		<div className="flex justify-between w-full bg-gray-100 rounded p-1.5 px-3">
+			<div className="flex items-center space-x-3 overflow-hidden">
+				<span className="text-gray-400 text-sm mr-2">{rank}</span>
+				<img className="rounded-full h-9 w-9 flex items-center justify-center" src={summonerIcon} alt="summoner icon" />
+				<div className="font-semibold text-lg text-gray-700 whitespace-nowrap">{name}</div>
+			</div>
+			<div className="flex items-center space-x-2">
+				<img className="rounded-full h-7 w-7 flex items-center justify-center" src={logo} alt="challanger" />
+				<div className="text-gray-700 font-semibold whitespace-nowrap">{lp} LP</div>
+				<div className="hidden items-center sm:flex space-x-2">
+					<div className="text-gray-500 text-sm">60%</div>
+					<div className="text-gray-500 text-sm">( 6W-4L )</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PlayerTab;
