cc37a849501ef224203286a59c6c89c7248a06b4
Firas Jaber <firasjaberc@gmail.com>
2021-08-10 22:58:09 +0100

    types for rankedstats and champions masteries

--- src/components/containers/Profile/Profile.tsx
+++ src/components/containers/Profile/Profile.tsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import ChampionsMastery from './ChampionsMastery';
 import Match from './Match';
 import RankedStats from './RankedStats';
 import RecentStats from './RecentStats';
 
+type League = {
+	leagueId: string;
+	queueType: string;
+	tier: string;
+	rank: string;
+	summonerId: string;
+	summonerName: string;
+	leaguePoints: number;
+	wins: number;
+	losses: number;
+	veteran: boolean;
+	inactive: boolean;
+	freshBlood: boolean;
+	hotStreak: boolean;
+};
+
 const Profile = () => {
+	const [rankedStats, setRankedStats] = useState<League | undefined>(undefined);
+	const [championsMastery, setChampionsMastery] = useState<Object[] | undefined>(undefined);
 	return (
 		<div className="flex flex-col items-center md:flex mb-60 lg:flex-row lg:items-start">
 			<div className="flex-initial w-auto mx-auto max-w-xl lg:max-64">
 				<div className="flex flex-col items-center md:flex-row md:space-x-2 lg:flex-col lg:space-x-0 lg:max-w-xs lg:mr-0 ">
-					<RankedStats league={undefined} />
-					<ChampionsMastery champions={undefined} />
+					<RankedStats league={rankedStats} />
+					<ChampionsMastery champions={championsMastery} />
 				</div>
 			</div>
 			<div className="flex-1 w-full max-w-xl mx-auto space-y-2 lg:mt-2.5">
