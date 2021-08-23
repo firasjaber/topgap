8f1a38a1a82cdc25c855c0ed6fd7097612e7aacb
Firas Jaber <firasjaberc@gmail.com>
2021-08-23 20:25:51 +0100

    header flex direction on small screens

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,7 +4,7 @@ import SearchInput from '../shared/SearchInput';
 const Header = () => {
 	return (
 		<nav className="pt-5 flex flex-col space-y-2 items-center justify-between sm:flex-row sm:space-y-0">
-			<div className="flex  space-x-8 items-center">
+			<div className="flex  flex-col space-y-3 mb-1  items-center sm:space-x-8 sm:flex-row sm:space-y-0 sm:mb-0">
 				<div className="font-exo font-bold text-3xl">
 					<span className="text-blue-950">TOP</span>
 					<span className="text-white">GAP</span>
