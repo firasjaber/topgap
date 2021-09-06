2b40906d26f05fbcdd87edc0f578283d831839c9
Firas Jaber <firasjaberc@gmail.com>
2021-09-06 23:45:40 +0100

    button styling

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,5 +1,12 @@
 const Button: React.FC = (props) => {
-	return <Button>{props.children}</Button>;
+	return (
+		<button
+			type="button"
+			className="px-4 py-3 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-75 transition-transform"
+		>
+			{props.children}
+		</button>
+	);
 };
 
 export default Button;
